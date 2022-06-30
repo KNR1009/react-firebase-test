9896e098afed5af3266cc8a148672c677afbd84f
伊東和成 <itokazunari5375@icloud.com>
2022-06-30 18:21:38 +0900

    ユーザー情報の登録

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,22 +1,75 @@
 /* Register.js */
 
-import React from "react";
+/* 「useEffect」をimport↓ */
+import React, { useState, useEffect } from "react";
+/* ↓「onAuthStateChanged」をimport */
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { auth } from "./lib/firebase";
+/* ↓「Navigate」をimport */
+import { Navigate } from "react-router-dom";
 
 const Register = () => {
+  const [registerEmail, setRegisterEmail] = useState("");
+  const [registerPassword, setRegisterPassword] = useState("");
+  /* ↓state変数「user」を定義 */
+  const [user, setUser] = useState<any>("");
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await createUserWithEmailAndPassword(
+        auth,
+        registerEmail,
+        registerPassword
+      );
+    } catch (error) {
+      alert("正しく入力してください");
+    }
+  };
+
+  /* ↓ログインしているかどうかを判定する */
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      setUser(user);
+      console.log(user);
+    });
+  }, []);
+
   return (
     <>
-      <h1>新規登録</h1>
-      <form>
-        <div>
-          <label>メールアドレス</label>
-          <input name="email" type="email" />
-        </div>
-        <div>
-          <label>パスワード</label>
-          <input name="password" type="password" />
-        </div>
-        <button>登録する</button>
-      </form>
+      {/* ↓ログインしていればマイページを表示 */}
+      {user ? (
+        <Navigate to={`/`} />
+      ) : (
+        <>
+          <h1>新規登録</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>メールアドレス</label>
+              <input
+                name="email"
+                type="email"
+                value={registerEmail}
+                onChange={(e) => setRegisterEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>パスワード</label>
+              <input
+                name="password"
+                type="password"
+                value={registerPassword}
+                onChange={(e) => setRegisterPassword(e.target.value)}
+              />
+            </div>
+            <button>登録する</button>
+          </form>
+        </>
+      )}
     </>
   );
 };
--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,20 +1,17 @@
-import { initializeApp, getApps } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+/* FirebaseConfig.js */
 
-import "firebase/analytics";
-import "firebase/auth";
-import "firebase/firestore";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-if (typeof window !== "undefined" && getApps().length === 0) {
-  const firebaseConfig = {
-    apiKey: "AIzaSyCTKa6rFKFw2pu_9B259-hjTrwWX3iAzJ0",
-    authDomain: "react-firebase-test-51090.firebaseapp.com",
-    projectId: "react-firebase-test-51090",
-    storageBucket: "react-firebase-test-51090.appspot.com",
-    messagingSenderId: "1053781311602",
-    appId: "1:1053781311602:web:6f419899cb9167563002eb",
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyCTKa6rFKFw2pu_9B259-hjTrwWX3iAzJ0",
+  authDomain: "react-firebase-test-51090.firebaseapp.com",
+  projectId: "react-firebase-test-51090",
+  storageBucket: "react-firebase-test-51090.appspot.com",
+  messagingSenderId: "1053781311602",
+  appId: "1:1053781311602:web:6f419899cb9167563002eb",
+};
 
-  initializeApp(firebaseConfig);
-  // getAnalytics();
-}
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
