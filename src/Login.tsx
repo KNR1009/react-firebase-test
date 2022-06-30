7b8a71c56a0cf7ddedb82f4c50db308af78d2e64
伊東和成 <itokazunari5375@icloud.com>
2022-06-30 18:29:25 +0900

    認証機能周りの実装

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,34 +1,61 @@
-import React, { useState } from "react";
+import { onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { Navigate } from "react-router";
+import { auth } from "./lib/firebase";
 
 const Login = () => {
   const [loginEmail, setLoginEmail] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [user, setUser] = useState<any>();
+
+  /* ↓関数「handleSubmit」を定義 */
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+
+    try {
+      await signInWithEmailAndPassword(auth, loginEmail, loginPassword);
+    } catch (error) {
+      alert("メールアドレスまたはパスワードが間違っています");
+    }
+  };
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  });
   return (
     <>
-      <h1>ログインページ</h1>
-      <form>
-        <div>
-          <label>メールアドレス</label>
-          {/* ↓「value」と「onChange」を追加 */}
-          <input
-            name="email"
-            type="email"
-            value={loginEmail}
-            onChange={(e) => setLoginEmail(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>パスワード</label>
-          {/* ↓「value」と「onChange」を追加 */}
-          <input
-            name="password"
-            type="password"
-            value={loginPassword}
-            onChange={(e) => setLoginPassword(e.target.value)}
-          />
-        </div>
-        <button>ログイン</button>
-      </form>
+      {user ? (
+        <Navigate to={`/`} />
+      ) : (
+        <>
+          <h1>ログインページ</h1>
+          <form onSubmit={handleSubmit}>
+            <div>
+              <label>メールアドレス</label>
+              {/* ↓「value」と「onChange」を追加 */}
+              <input
+                name="email"
+                type="email"
+                value={loginEmail}
+                onChange={(e) => setLoginEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label>パスワード</label>
+              {/* ↓「value」と「onChange」を追加 */}
+              <input
+                name="password"
+                type="password"
+                value={loginPassword}
+                onChange={(e) => setLoginPassword(e.target.value)}
+              />
+            </div>
+            <button>ログイン</button>
+          </form>
+        </>
+      )}
     </>
   );
 };
--- src/Mypage.tsx
+++ src/Mypage.tsx
@@ -1,10 +1,32 @@
-import React from "react";
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
+import { auth } from "./lib/firebase";
 
 const MyPage = () => {
+  const [user, setUser] = useState<any>("");
+
+  /* ↓ログインしているかどうかを判定する */
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+    });
+  }, []);
+
+  /* ↓「navigate」を定義 */
+  const navigate = useNavigate();
+
+  /* ↓関数「logout」を定義 */
+  const logout = async () => {
+    await signOut(auth);
+    navigate("/login/");
+  };
+
   return (
     <>
       <h1>マイページ</h1>
-      <button>ログアウト</button>
+      <p>{user?.email}</p>
+      <button onClick={logout}>ログアウト</button>
     </>
   );
 };
